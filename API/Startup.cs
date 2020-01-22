7ae67c072b2ca86d30d484c56f9beaf43db9c444
Sercan <sercan@Sercans-MacBook-Pro.local>
2020-01-22 13:13:20 +0300

    Activity CQRS and Controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,9 @@ namespace API
             {
                 opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+
             services.AddControllers();
         }
 
