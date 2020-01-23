6349b1580a13817b3d7046bbc1698651d5163952
Sercan <sercan@Sercans-MacBook-Pro.local>
2020-01-23 15:04:39 +0300

    Middleware error handler and validation

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -11,21 +12,28 @@ namespace Application.Activities
     {
         public class Query : IRequest<Activity>
         {
-            public Guid Id { get; set;}
+            public Guid Id { get; set; }
+        }
 
-            public class Handler : IRequestHandler<Query,Activity>
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
             {
-                private readonly DataContext _context;
-                public Handler(DataContext context)
-                {
-                    _context = context;
-                }
+                _context = context;
+            }
 
-                public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
-                {
-                    var activity = await _context.Activities.FindAsync(request.Id);
-                    return activity;
-                }
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new RestException(System.Net.HttpStatusCode.NotFound, new
+                    {
+                        activity = "Not Found"
+                    });
+
+                return activity;
             }
         }
     }
