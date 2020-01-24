44f5a173a06b5921b7fb135c08351bd9559e929d
Sercan <sercan@Sercans-MacBook-Pro.local>
2020-01-25 00:00:10 +0300

    Added .Net Core Identity

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Domain
 {
-    public class AppUser
+    public class AppUser : IdentityUser
     {
-        
+     public string DisplayName { get; set; }   
     }
 }
